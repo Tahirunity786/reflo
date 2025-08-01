c3f83d56d920bdaba6d0eb946282214fc6c03848
Tahir Zaman <tahirunity786@gmail.com>
2025-08-02 02:02:01 +0500

    update for django

--- src/lib/queue.js
+++
@@ -1,6 +0,0 @@
-// lib/queue.js
-import { Queue } from 'bullmq';
-import IORedis from 'ioredis';
-
-const connection = new IORedis(); // configure Redis connection
-export const productQueue = new Queue('productQueue', { connection });
