3dccb11a9f3b01c5c12514c171bb9b60b11da8a5
Tahir Zaman <tahirunity786@gmail.com>
2025-09-01 12:17:40 +0500

    Updated checkout

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,6 +53,32 @@ export default function RootLayout({ children }) {
           })(window, document, "clarity", "script", "t0gw1glx7b");
         `}
       </Script>
+
+      {/* Google Tag Manager */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KPRFLHJG');
+        `}
+      </Script>
+      {/* End Google Tag Manager */}
+
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-N4KMGR17NW"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-script" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-N4KMGR17NW');
+        `}
+      </Script>
       </body>
     </html>
   );
