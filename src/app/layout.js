5fda1202cde80ffd2e741c55e97fe268c0632fd4
Tahir Zaman <tahirunity786@gmail.com>
2025-09-18 12:13:40 +0500

    update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
 
           <ConditionalWrapper>{children}</ConditionalWrapper>
         </ReduxProvider>
-
+        <FacebookPixel />
         <Script
           id="tawk-to"
           strategy="afterInteractive"
@@ -48,11 +48,11 @@ export default function RootLayout({ children }) {
         >
           {`
           (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "t0gw1glx7b");
-            `}
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "t0gw1glx7b");
+        `}
         </Script>
 
         {/* Google Tag Manager */}
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-KPRFLHJG');
-          `}
+        `}
         </Script>
         {/* End Google Tag Manager */}
 
@@ -78,9 +78,8 @@ export default function RootLayout({ children }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-N4KMGR17NW');
-          `}
+        `}
         </Script>
-        <FacebookPixel />
       </body>
     </html>
   );
--- src/components/FacebookPixel/FacebookPixel.jsx
+++ src/components/FacebookPixel/FacebookPixel.jsx
@@ -1,42 +1,49 @@
-// components/FacebookPixel.jsx
-'use client';
-import { useEffect } from 'react';
-import Script from 'next/script';
-import { usePathname } from 'next/navigation';
-import { pageview } from '@/lib/fpixel';
+"use client";
+
+import { useEffect } from "react";
+import Script from "next/script";
+import { usePathname } from "next/navigation";
 
 export default function FacebookPixel() {
-  const PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
+  const PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID || "753376781013461";
   const pathname = usePathname();
 
-  // track page views on route change
+  // fire PageView on route change
   useEffect(() => {
-    // wait until fbq is ready
-    if (typeof window !== 'undefined' && window.fbq) {
-      pageview();
+    if (typeof window.fbq !== "undefined") {
+      window.fbq("track", "PageView");
     }
   }, [pathname]);
 
-  if (!PIXEL_ID) return null;
-
   return (
     <>
-      {/* load the FB events script and initialize */}
       <Script
         id="fb-pixel-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${PIXEL_ID}');
-        `,
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+              n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
         }}
       />
-      {/* we trigger PageView on route change via useEffect above */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+          alt="fb-pixel"
+        />
+      </noscript>
     </>
   );
 }
